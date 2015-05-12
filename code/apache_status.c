b7c8f700d2f093fa89f7cd5c766d4b02d879b672
pincopallino93 <pastorini.claudio@gmail.com>
2015-05-12 18:19:43 +0200

    Created helper code with some useful macro and functions, improved documentation of apache_status.

--- code/apache_status.c
+++ code/apache_status.c
@@ -1,17 +1,40 @@
-
-// http://httpd.apache.org/docs/2.4/mod/mod_status.html
+//
+//============================================================================
+// Name       : apache_status.c
+// Author     : Claudio Pastorini
+// Version    : 0.1
+// Description: This file contains all the stuffs useful in order to
+//              reads the status of an Apache server.
+//              For other info read:
+//               - http://httpd.apache.org/docs/2.4/mod/mod_status.html
+// ===========================================================================
+//
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <errno.h>
+#include "helper.h"
 
 /*
+ * ---------------------------------------------------------------------------
  * Structure:  apache_server_status
- * --------------------
- * Description
+ * Description: This struct collect all data about the state of a Apace server.
  *
- *
+ * Data:
+ *   url           : The URL of the server status page.
+ *   total_accesses: A total number of accesses served.
+ *   total_kBytes  : A total number of byte count served.
+ *   cpu_load      : The current percentage CPU used by each worker and in total
+ *                   by all workers combined
+ *   uptime        : The time the server was started/restarted and the time it
+ *                   has been running for.
+ *   req_per_sec   : Averages giving the number of requests per second, the
+ *                   number of bytes served per second and the average number
+ *                   of bytes per request.
+ *   bytes_per_sec : The number of bytes served per second.
+ *   bytes_per_req : The average number of bytes per request.
+ *   busy_workers  : The number of worker serving requests.
+ *   idle_workers  : The number of idle worker.
  */
 struct apache_server_status {
     char *url;
@@ -27,70 +50,45 @@ struct apache_server_status {
 };
 
 /*
- * Function:  retrieve_apache_status
- * --------------------
- * Description
+ * ---------------------------------------------------------------------------
+ * Function:     retrieve_apache_status
+ * Description:  This function retrieve the HTML page of the given URL. It
+ *               save the page into status_page.
  *
- *  url:
- *  status_page:
+ * Param:
+ *   url        : The URL of the page to retrieve.
+ *   status_page: The pointer in which save the page retrieved.
  *
- *  returns:
+ * Return:       STATUS_OK in case of success, STATUS_ERROR otherwise.
+ * ---------------------------------------------------------------------------
  */
 int retrieve_apache_status(char *url, char **status_page) {
 
     *status_page = "Total Accesses: 143\nTotal kBytes: 340\nCPULoad: .125764\nUptime: 1145\nReqPerSec: .124891\nBytesPerSec: 304.07\nBytesPerReq: 2434.69\nBusyWorkers: 1\nIdleWorkers: 7\nScoreboard: _____W__..............................................................................................................................................";
 
-    return 1;
-}
-
-int str_to_int(char *string) {
-
-    char *pointer;
-    int value;
-
-    errno = 0;
-    value = (int) strtol(string, &pointer, 0);
-    if (errno != 0 || *pointer != '\0') {
-        fprintf(stderr, "Invalid number");
-        return -1;
-    }
-
-    return value;
-}
-
-float str_to_float(char *string) {
-
-    char *pointer;
-    float value;
-
-    errno = 0;
-    value =  strtof(string, &pointer);
-    if (errno != 0 || *pointer != '\0') {
-        fprintf(stderr, "Invalid number");
-        return -1;
-    }
-
-    return value;
+    return STATUS_OK;
 }
 
 /*
- * Function:  parse_apache_status
- * --------------------
- * Description
+ * ---------------------------------------------------------------------------
+ * Function:     parse_apache_status
+ * Description:  This function parse the HTML page given in order to
+ *               get all the info about Apache server.
  *
- *  status_page:
- *  server_status:
+ * Param:
+ *   status_page  : The pointer in which is saved the page to parse.
+ *   server_status: The struct in which save the data parsed.
  *
- *  returns:
+ * Return:       STATUS_OK in case of success, STATUS_ERROR otherwise.
+ * ---------------------------------------------------------------------------
  */
 int parse_apache_status(char **status_page, struct apache_server_status* server_status) {
 
-
-    char *to_parse = strdup(*status_page);
-    char *delim = ":";
-    char *sub_delim = "\n";
-    char *str1, *str2, *token, *sub_token;
-    char *save_ptr1, *save_ptr2;
+    char *to_parse = strdup(*status_page);  // The string to parse
+    char *delim = ":";  // First delimiter
+    char *sub_delim = "\n";  // Second delimiter
+    char *str1, *str2, *token, *sub_token;  // Useful string
+    char *save_ptr1, *save_ptr2;  // Useful pointer
     int j, i;
 
     for (j = 1, i = 0, str1 = to_parse; ; j++, str1 = NULL) {
@@ -98,7 +96,6 @@ int parse_apache_status(char **status_page, struct apache_server_status* server_
         if (token == NULL) {
             break;
         }
-        //printf("%d: %s\n", j, token);
 
         for (str2 = token; ; str2 = NULL) {
             sub_token = strtok_r(str2, sub_delim, &save_ptr2);
@@ -107,125 +104,128 @@ int parse_apache_status(char **status_page, struct apache_server_status* server_
             }
             i++;
             if (i % 2 == 0) {
-                //printf("%d: %s\n", i, sub_token);
                 switch (i) {
-                    case 2: {
+                    case 2: {  // Total Accesses
                         int total_accesses = str_to_int(sub_token);
 
-                        if (total_accesses != -1) {
+                        if (total_accesses != STATUS_ERROR) {
                             server_status->total_accesses = total_accesses;
                         } else {
-                            fprintf(stderr, "Unable to parse");
-                            return -1;
+                            fprintf(stderr, "Unable to parse\n");
+                            return STATUS_ERROR;
                         }
-                    }
                         break;
-                    case 4: {
+                    }
+                    case 4: {  // Total kBytes
                         int total_kBytes = str_to_int(sub_token);
 
-                        if (total_kBytes != -1) {
+                        if (total_kBytes != STATUS_ERROR) {
                             server_status->total_kBytes = total_kBytes;
                         } else {
-                            fprintf(stderr, "Unable to parse");
-                            return -1;
+                            fprintf(stderr, "Unable to parse\n");
+                            return STATUS_ERROR;
                         }
-                    }
                         break;
-                    case 6: {
+                    }
+                    case 6: {  // CPULoad
                         float cpu_load = str_to_float(sub_token);
 
-                        if (cpu_load != -1) {
+                        if (cpu_load != STATUS_ERROR) {
                             server_status->cpu_load = cpu_load;
                         } else {
-                            fprintf(stderr, "Unable to parse");
-                            return -1;
+                            fprintf(stderr, "Unable to parse\n");
+                            return STATUS_ERROR;
                         }
-                    }
                         break;
-                    case 8: {
+                    }
+                    case 8: {  // Uptime
                         int uptime = str_to_int(sub_token);
 
-                        if (uptime != -1) {
+                        if (uptime != STATUS_ERROR) {
                             server_status->uptime = uptime;
                         } else {
-                            fprintf(stderr, "Unable to parse");
-                            return -1;
+                            fprintf(stderr, "Unable to parse\n");
+                            return STATUS_ERROR;
                         }
-                    }
                         break;
-                    case 10: {
+                    }
+                    case 10: {  // ReqPerSec
                         float req_per_sec = str_to_float(sub_token);
 
-                        if (req_per_sec != -1) {
+                        if (req_per_sec != STATUS_ERROR) {
                             server_status->req_per_sec = req_per_sec;
                         } else {
-                            fprintf(stderr, "Unable to parse");
+                            fprintf(stderr, "Unable to parse\n");
                             return -1;
                         }
-                    }
                         break;
-                    case 12: {
+                    }
+                    case 12: {  // BytesPerSec
                         float bytes_per_sec = str_to_float(sub_token);
 
-                        if (bytes_per_sec != -1) {
+                        if (bytes_per_sec != STATUS_ERROR) {
                             server_status->bytes_per_sec = bytes_per_sec;
                         } else {
-                            fprintf(stderr, "Unable to parse");
-                            return -1;
+                            fprintf(stderr, "Unable to parse\n");
+                            return STATUS_ERROR;
                         }
-                    }
                         break;
-                    case 14: {
+                    }
+                    case 14: {  // BytesPerReq
                         float bytes_per_req = str_to_float(sub_token);
 
-                        if (bytes_per_req != -1) {
+                        if (bytes_per_req != STATUS_ERROR) {
                             server_status->bytes_per_req = bytes_per_req;
                         } else {
-                            fprintf(stderr, "Unable to parse");
-                            return -1;
+                            fprintf(stderr, "Unable to parse\n");
+                            return STATUS_ERROR;
                         }
-                    }
                         break;
-                    case 16: {
+                    }
+                    case 16: {  // BusyWorkers
                         int busy_workers = str_to_int(sub_token);
 
-                        if (busy_workers != -1) {
+                        if (busy_workers != STATUS_ERROR) {
                             server_status->busy_workers = busy_workers;
                         } else {
-                            fprintf(stderr, "Unable to parse");
-                            return -1;
+                            fprintf(stderr, "Unable to parse\n");
+                            return STATUS_ERROR;
                         }
-                    }
                         break;
-                    case 18: {
+                    }
+                    case 18: {  // IdleWorkers
                         int idle_workers = str_to_int(sub_token);
 
-                        if (idle_workers != -1) {
+                        if (idle_workers != STATUS_ERROR) {
                             server_status->idle_workers = idle_workers;
                         } else {
-                            fprintf(stderr, "Unable to parse");
-                            return -1;
+                            fprintf(stderr, "Unable to parse\n");
+                            return STATUS_ERROR;
                         }
-                    }
                         break;
+                    }
                     default:
                         break;
                 }
             }
         }
     }
+    // Free memory used for to_parse
+    free(to_parse);
 
-    return 1;
+    return STATUS_OK;
 }
 
 /*
- * Function:  print_apache_status
- * --------------------
- * Description
+ * ---------------------------------------------------------------------------
+ * Function:     print_apache_status
+ * Description:  This function print the status stored into
+ *               apache_server_status struct.
  *
- *  server_status:
+ * Param:
+ *   server_status : The pointer of the struct to print.
  *
- *  returns:
+ * ---------------------------------------------------------------------------
  */
 void print_apache_status(struct apache_server_status* server_status) {
     fprintf(stdout, "Status of Apache server at URL: %s\n\nTotal Accesses: %d\nTotal kBytes: %d\nCPULoad: %f\nUptime: "
@@ -267,7 +267,7 @@ int main(int argc, char *argv[]) {
     status_retrieve = retrieve_apache_status(server_status->url, status_page);
 
     int status_parse;
-    if (status_retrieve) {  // If retrieved
+    if (status_retrieve == STATUS_OK) {  // If retrieved
         status_parse = parse_apache_status(status_page, server_status);  // Parse status_page and put into server_status
     } else {
         fprintf(stderr, "Error in retriving status page, please retry.\n");
@@ -277,7 +277,7 @@ int main(int argc, char *argv[]) {
     // Free memory used for status_page
     free(status_page);
 
-    if (status_parse) {  // If parsed
+    if (status_parse == STATUS_OK) {  // If parsed
         print_apache_status(server_status);  // Print server_status
     } else {
         fprintf(stderr, "Error in parsing status page, please retry.\n");
---
+++ code/helper.h
@@ -0,0 +1,88 @@
+//
+//============================================================================
+// Name       : utils.h
+// Author     : Claudio Pastorini
+// Version    : 0.1
+// Description: Header file with some useful macro and functions.
+// ===========================================================================
+//
+
+#include <errno.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <err.h>
+
+#define STATUS_OK 0  // Macro for status ok
+#define STATUS_ERROR -1  // Macro for status error
+
+#define TRUE 1  // Macro for true
+#define FALSE 0  // Macro for false
+
+// TODO define a way to return message error
+#define VERBOSE_LEVEL 2  // Macro for verbose message level
+#define DEBUG_LEVEL 3  // Macro for debug message level
+#define INFO_LEVEL 4  // Macro for info message level
+#define WARN_LEVEL 5  // Macro for warn message level
+#define ERROR_LEVEL 6  // Macro for error message level
+
+/*
+ *
+ */
+struct message {
+    int code;
+    int level;
+    char *message;
+};
+/*
+ * ---------------------------------------------------------------------------
+ * Function:     str_to_int
+ * Description:  This function converts a string into a int value using the
+ *               C command strotol.
+ *
+ * Param:
+ *   string:     The string to convert.
+ *
+ * Return:       The converted value or STATUS_ERROR in case of error.
+ * ---------------------------------------------------------------------------
+ */
+int str_to_int(char *string) {
+
+    char *pointer;
+    int value;
+
+    errno = 0;
+    value = (int) strtol(string, &pointer, 0);
+    if (errno != 0 || *pointer != '\0') {
+        fprintf(stderr, "Invalid number");
+        return STATUS_ERROR;
+    }
+
+    return value;
+}
+
+/*
+ * ---------------------------------------------------------------------------
+ * Function:     str_to_float
+ * Description:  This function converts a string into a float value using the
+ *               C command strotof.
+ *
+ * Param:
+ *   string:     The string to convert.
+ *
+ * Return:       The converted value or STATUS_ERROR in case of error.
+ * ---------------------------------------------------------------------------
+ */
+float str_to_float(char *string) {
+
+    char *pointer;
+    float value;
+
+    errno = 0;
+    value = strtof(string, &pointer);
+    if (errno != 0 || *pointer != '\0') {
+        fprintf(stderr, "Invalid number");
+        return STATUS_ERROR;
+    }
+
+    return value;
+}

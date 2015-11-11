be33a6d97ad4a1fa202822dd7d0d763bdd8bd97d
Alessio Moretti <alessio.moretti@live.it>
2015-11-11 12:03:45 +0100

    created worker.c / worker.h

---
+++ code/web_switch/worker.c
@@ -0,0 +1,12 @@
+//
+//============================================================================
+// Name             : worker.c
+// Author           : Alessio Moretti e Claudio Pastorini
+// Version          : 0.2
+// Data Created     : 11/11/2015
+// Last modified    : 11/11/2015
+// Description      : This is the webswitch single Worker thread body
+// ===========================================================================
+//
+
+#include "worker.h"

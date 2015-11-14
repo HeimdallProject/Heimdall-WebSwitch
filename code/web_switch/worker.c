add142df7a24c5f60d8d08933799740d6144e216
Alessio Moretti <alessio.moretti@live.it>
2015-11-14 18:48:38 +0100

    watchdog first structure example (dev variable to be edited!)

--- code/web_switch/worker.c
+++ code/web_switch/worker.c
@@ -13,30 +13,103 @@
 #include <string.h>
 #include <unistd.h>
 #include <time.h>
+#include <errno.h>
 #include <pthread.h>
 
 #include "worker.h"
 
+
+// initializing the watchdog struct
+Watchdog *watchdog;
+
+// initializing the requests buffer (DEV)
+ThreadRequest *requests;
+
 int start_worker() {
-    time_t timestamp;
 
     // initializing write thread
-    // ** do something to start the thread**
+    // TODO: do something to start the thread
 
-    // initializing watchdog
-    // ** do something to start the thread **
+    // setting up watchdog
+    // struct allocation
+    watchdog = malloc(sizeof(Watchdog));
+    if (watchdog == NULL)
+        return STATUS_ERROR;
+    // struct init
+    watchdog->requests = requests;
+    // thread initialization
+    int watchdog_creation = pthread_create(watchdog->thread_id, NULL, enable_watchdog, NULL);
+    if (watchdog_creation != 0)
+        return STATUS_ERROR;
 
     // initialilizing the FIFO data structure to manage a
     // node of the current request handled (pipeline-robust approach)
-    // ** do something to allocate the FIFO **
+    // TODO: do something to allocate the FIFO
 
     // starting the main routine cycling in a read/execute loop
-    while(TRUE) {
-        // ** reading and passing params to HTTPRequest setting the timestamp **/
-        timestamp = time(NULL);
+    for (;;) {
+        // TODO: reading and passing params to HTTPRequest setting the timestamp foreach request in the FIFO
+
 
     }
 
     // successfull status
     return STATUS_OK;
 }
+
+
+void *enable_watchdog(void *arg) {
+
+    // initializing time specifics
+    struct timespec *req_time = malloc(sizeof(struct timespec));
+    struct timespec *rem_time = malloc(sizeof(struct timespec));
+    if (req_time == NULL || rem_time == NULL)
+        return (void *) (intptr_t) STATUS_ERROR;
+
+
+    // starting watch-over-thread loop main routine
+    int sleep_status;
+    int watch_status;
+    for (;;) {
+        // TODO: advancing step
+        // sleeping loop
+        // setting req_time specifics
+        req_time->tv_nsec = watchdog->killer_time;
+        while (TRUE) {
+            sleep_status = nanosleep(req_time, rem_time);
+            // upon not successfull complete nanosleep
+            if (sleep_status == -1) {
+                if (errno == EFAULT)
+                    return (void *) (intptr_t) STATUS_ERROR;
+                if (errno == EINTR)
+                    req_time->tv_nsec = rem_time->tv_nsec;
+            }
+            // upon successfull complete nanosleep -> watchover routine
+            if (sleep_status == 0)
+                break;
+        }
+
+        // watchover routine
+        watch_status = watch_over(watchdog->requests->thread_id, watchdog->requests->timestamp, time(NULL));
+        if (watch_status == STATUS_ERROR)
+            return (void *) (intptr_t) STATUS_ERROR;
+    }
+
+    // successfull status
+    return (void *) (intptr_t) STATUS_OK;
+}
+
+int watch_over(pthread_t *running_thread, time_t running_timestamp, time_t current_timestamp) {
+
+    // checking for timestamp distance and aborting thread if necessary
+    time_t running_exec_time = current_timestamp - running_timestamp;
+    if (running_exec_time > watchdog->exec_time) {
+        int cancellation_status = pthread_cancel(*running_thread);
+        if (cancellation_status != 0)
+            return STATUS_ERROR;
+    }
+
+    // successfull status
+    return STATUS_OK;
+}
+
--- code/web_switch/worker.h
+++ code/web_switch/worker.h
@@ -11,7 +11,40 @@
 #ifndef WEBSWITCH_WORKER_H
 #define WEBSWITCH_WORKER_H
 
+#include <pthread.h>
+#include <time.h>
 #include "../utils/helper.h"
+
+
+
+/*
+ * ---------------------------------------------------------------------------
+ * Structure        : typedef struct thread_request
+ * Description      : this struct manage the request
+ * ---------------------------------------------------------------------------
+ */
+typedef struct thread_request {
+    pthread_t *thread_id;                            // thread identifier
+    time_t timestamp;                               // timestamp for watchdog check
+} ThreadRequest;
+
+
+/*
+ * ---------------------------------------------------------------------------
+ * Structure        : typedef struct watchdog_thread
+ * Description      : this struct helps to manage and set attributes for the thread
+ *                    which watch over the remote connection thread termination
+ * ---------------------------------------------------------------------------
+ */
+typedef struct watchdog_thread {
+    pthread_t *thread_id;                           // thread identifier
+    long killer_time;                               // time to schedule the watchdog wakeup
+    time_t exec_time;                               // time to abort a thread run
+
+    struct thread_request *requests;                // pointer to the request array (DEV)
+} Watchdog;
+
+
 
 /*
  * ---------------------------------------------------------------------------
@@ -24,5 +57,27 @@
  * ---------------------------------------------------------------------------
  */
 int start_worker();
+
+/*
+ * ---------------------------------------------------------------------------
+ * Function   : enable_watchdog
+ * Description: This function runs the watchdog routine
+ *
+ * Param      :
+ * Return     : STATUS_OK on successfull operations status, STATUS_ERROR otherwise
+ * ---------------------------------------------------------------------------
+ */
+void *enable_watchdog(void *arg);
+
+/*
+ * ---------------------------------------------------------------------------
+ * Function   : watch_over
+ * Description: This function runs the watcher routine over the request threads
+ *
+ * Param      :
+ * Return     : STATUS_OK on successfull operations status, STATUS_ERROR otherwise
+ * ---------------------------------------------------------------------------
+ */
+int watch_over(pthread_t *running_thread, time_t running_timestamp, time_t current_timestamp);
 
 #endif //WEBSWITCH_WORKER_H

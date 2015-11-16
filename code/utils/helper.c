d46a6a56313de9fd8d1419decc2f65195dd59eae
Andrea Cerra <andrea.cerra@me.com>
2015-11-16 19:06:59 +0100

    Added a new version for timestamp function. Removed \n from returned string.

--- code/utils/helper.c
+++ code/utils/helper.c
@@ -15,35 +15,50 @@
 #include <ctype.h>
 #include <string.h>
 #include <limits.h>
-#include "helper.h"
 #include "time.h"
 #include "log.h"
+#include "helper.h"
 
+/*
+ *  See .h for more information.
+ */
 Throwable *str_to_int(const char *string, int *value) {
     char *pointer;
 
     errno = 0;
     *value = (int) strtol(string, &pointer, 0);
 
-    if ((errno == ERANGE) || (errno != 0 && *value == 0)) return (*get_throwable()).create(STATUS_ERROR, get_error_by_errno(errno), "str_to_int");
-    if (pointer == string) return (*get_throwable()).create(STATUS_ERROR, "No digits were found", "str_to_int");
-    if (*pointer != '\0') return (*get_throwable()).create(STATUS_ERROR, "There is some characters", "str_to_int");
+    if ((errno == ERANGE) || (errno != 0 && *value == 0)) 
+        return (*get_throwable()).create(STATUS_ERROR, get_error_by_errno(errno), "str_to_int");
+    if (pointer == string) 
+        return (*get_throwable()).create(STATUS_ERROR, "No digits were found", "str_to_int");
+    if (*pointer != '\0') 
+        return (*get_throwable()).create(STATUS_ERROR, "There is some characters", "str_to_int");
 
     return (*get_throwable()).create(STATUS_OK, NULL, "str_to_int");
 }
 
+/*
+ *  See .h for more information.
+ */
 Throwable *str_to_float(char *string, float *value) {
     char *pointer;
 
     errno = 0;
     *value = strtof(string, &pointer);
-    if ((errno == ERANGE && (*value == LONG_MAX || *value == LONG_MIN)) || (errno != 0 && *value == 0)) return (*get_throwable()).create(STATUS_ERROR, get_error_by_errno(errno), "str_to_float");
-    if (pointer == string) return (*get_throwable()).create(STATUS_ERROR, "No digits were found", "str_to_float");
-    if (*pointer != '\0') return (*get_throwable()).create(STATUS_ERROR, "There is some characters", "str_to_float");
+    if ((errno == ERANGE && (*value == LONG_MAX || *value == LONG_MIN)) || (errno != 0 && *value == 0)) 
+        return (*get_throwable()).create(STATUS_ERROR, get_error_by_errno(errno), "str_to_float");
+    if (pointer == string) 
+        return (*get_throwable()).create(STATUS_ERROR, "No digits were found", "str_to_float");
+    if (*pointer != '\0') 
+        return (*get_throwable()).create(STATUS_ERROR, "There is some characters", "str_to_float");
 
     return (*get_throwable()).create(STATUS_OK, NULL, "str_to_float");
 }
 
+/*
+ *  See .h for more information.
+ */
 char *str_to_lower(char *string) {
     int i;
     for (i = 0; string[i]; i++) {
@@ -53,6 +68,9 @@ char *str_to_lower(char *string) {
     return string;
 }
 
+/*
+ *  See .h for more information.
+ */
 char *get_error_by_errno(const int error) {
     char error1[250];
     snprintf(error1, 250, "%d", error);
@@ -75,12 +93,34 @@ char *get_error_by_errno(const int error) {
     return message;
 }
 
+/*
+ *  See .h for more information.
+ */
 char *timestamp() {
+    
     // Get current time
     time_t current_time;
     current_time = time(NULL);
-    // Returns in a human readable way
-    return asctime(localtime(&current_time));
+    
+    // Get a human readable way
+    char *timestamp = asctime(localtime(&current_time));
+
+    int count = 0;
+    while (1) {
+        if (timestamp[count] == '\n'){
+            timestamp[count] = '\0';
+            break;
+        }
+        ++count;
+    }
+    
+    return timestamp;
 }
 
-//TODO fare funzione che rimuove (che fa il replace) \n, da usare in timestamp
+
+
+
+
+
+
+

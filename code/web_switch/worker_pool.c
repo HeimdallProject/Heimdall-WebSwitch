56006103a0719778949286c542e539b9f5ef0590
Andrea Cerra <andrea.cerra@me.com>
2015-11-17 11:27:46 +0100

    Added worker_pool files.

---
+++ code/web_switch/worker_pool.c
@@ -0,0 +1,12 @@
+//
+//============================================================================
+// Name             : thread_pool.c
+// Author           : Andrea Cerra
+// Version          : 0.1
+// Data Created     : 17/11/2015
+// Last modified    : 17/11/2015
+// Description      : This file contains the implementation for a Linked List.
+// ===========================================================================
+//
+
+#include "worker_pool.h"
---
+++ code/web_switch/worker_pool.h
@@ -0,0 +1,23 @@
+//
+//============================================================================
+// Name             : thread_pool.h
+// Author           : Andrea Cerra
+// Version          : 0.1
+// Data Created     : 17/11/2015
+// Last modified    : 17/11/2015
+// Description      : This header infomation about worker pool object.
+// ===========================================================================
+//
+
+#ifndef WORKER_POOL_H
+#define WORKER_POOL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#define TAG_WORKER_POOL_H "WORKER_POOL"
+
+
+
+#endif

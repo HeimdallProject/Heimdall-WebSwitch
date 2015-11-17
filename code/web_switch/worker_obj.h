90cbc8012b6c393ce4fffd511cebced76e466d6c
Andrea Cerra <andrea.cerra@me.com>
2015-11-17 11:27:27 +0100

    Created a basic implementation of Worker object. Maybe this code will have to be merged with worker.c/h

---
+++ code/web_switch/worker_obj.h
@@ -0,0 +1,48 @@
+//
+//============================================================================
+// Name             : worker_obj.h
+// Author           : Andrea Cerra
+// Version          : 0.1
+// Data Created     : 16/11/2015
+// Last modified    : 16/11/2015
+// Description      : This header file contains infomation about worker object.
+// ===========================================================================
+//
+
+#ifndef WORKER_OBJ_H
+#define WORKER_OBJ_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Macro for log message
+#define TAG_WORKER_OBJ "WORKER_OBJ"
+
+/*
+ * ---------------------------------------------------------------------------
+ * Structure    : typedef struct worker
+ * Description  : 
+ *
+ * Data:
+ *
+ * Functions:
+ * ---------------------------------------------------------------------------
+ */
+typedef struct worker {
+    char* thread_identifier;
+    char* process_identifier;
+} Worker;
+
+/*
+ * ---------------------------------------------------------------------------
+ * Function   : new_worker
+ * Description: Method for worker creation. 
+ *
+ * Param      :
+ *
+ * Return     :
+ * ---------------------------------------------------------------------------
+ */
+Worker *new_worker();
+
+#endif

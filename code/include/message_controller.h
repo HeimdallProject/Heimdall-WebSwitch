a2f59ce22f854f111339cf3f434208792f7f684d
Andrea Cerra <andrea.cerra@me.com>
2015-11-27 10:10:07 +0100

    First implementation of message controller. - The file descriptor are shared from different process trough a UNIX socket.

---
+++ code/include/message_controller.h
@@ -0,0 +1,27 @@
+//
+//============================================================================
+// Name       	: message_controller.h
+// Description	: This program sends a file descriptor to a UNIX domain socket.
+// ===========================================================================
+//
+#ifndef MESSAGE_CONTROLLER_H
+#define MESSAGE_CONTROLLER_H
+
+/* To get SCM_CREDENTIALS definition from <sys/sockets.h> */
+#define _GNU_SOURCE
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include "../include/unix_socket.h"
+
+#define TAG_MESSAGE_CONTROLLER "MESSAGE_CONTROLLER"
+
+void receive_fd();
+void send_fd(int fd);
+
+#endif //MESSAGE_CONTROLLER_H
